c1db1ef21e1272ec8dcdf1551503fe3ad37308dd
matheuzzu <mathlimfon@gmail.com>
2022-10-12 16:04:07 -0300

    portfolio-init

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 20) $(".navbar").addClass("sticky");
+    else $(".navbar").removeClass("sticky");
+  });
+
+  $(".menu-toggler").click(function () {
+    $(this).toggleClass("active");
+    $(".nav-menu").toggleClass("active");
+  });
+});
